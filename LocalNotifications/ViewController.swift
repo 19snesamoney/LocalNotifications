f5162e54e57c9f8d67a11324e7c1d147d2a674bc
sophia.nesamoney <sophia.nesamoney@gmail.com>
2018-03-05 09:49:29 -0800

    Code for setting date and time is commented out

--- LocalNotifications/ViewController.swift
+++ LocalNotifications/ViewController.swift
@@ -7,12 +7,39 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
+    //var year = 2018
+   // var month = 09
+   //var day = 05
+   // var hour = 09
+    //var minute = 41
+   // var second = 03
 
+    @IBAction func StartNotification(_ sender: Any) {
+        let content = UNMutableNotificationContent()
+        content.title = "Meeting Reminder"
+        content.body = "Please go to the library for your meeting at 3:00pm"
+        content.badge = 1
+        
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+        //let date = Date(timeIntervalSinceNow: 3600)
+       // let triggerDate = Calendar.current.dateComponents([.year, .month, .day, .hour, .minute, .second], from: date)
+       // let trigger = UNCalendarNotificationTrigger(dateMatching: triggerDate, repeats: false)
+        
+        let request = UNNotificationRequest(identifier: "timerDone", content: content, trigger: trigger)
+        UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
+
+
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge], completionHandler: {didAllow, error in
+            
+        })
+        
     }
 
     override func didReceiveMemoryWarning() {
